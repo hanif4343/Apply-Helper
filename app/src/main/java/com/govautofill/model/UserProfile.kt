780bf161d142658b1e8c558595eb53a4f99092e8
hanif4343 <hsarder43@gmail.com>
2026-06-23 16:52:43 +0600

    Add files via upload

--- app/src/main/java/com/govautofill/model/UserProfile.kt
+++ app/src/main/java/com/govautofill/model/UserProfile.kt
@@ -12,20 +12,32 @@ data class UserProfile(
     var motherNameEn: String = "",
     var spouseNameBn: String = "",
     var spouseNameEn: String = "",
+    var careOf: String = "",
 
     // Birth Info
     var dateOfBirth: String = "",        // DD/MM/YYYY
     var placeOfBirth: String = "",
-    var birthCertificateNo: String = "",
+    var birthCertificateNo: String = "", // Birth Registration Number
     var nidNo: String = "",
     var passportNo: String = "",
 
     // Contact
     var mobileNo: String = "",
+    var confirmMobileNo: String = "",
     var alternatePhone: String = "",
     var email: String = "",
 
+    // Identity
+    var religion: String = "",
+    var nationality: String = "Bangladeshi",
+    var gender: String = "",
+    var maritalStatus: String = "",
+    var quota: String = "Not Applicable",
+    var departmentalStatus: String = "Not Applicable",
+    var bloodGroup: String = "",
+
     // Address - Present
+    var presentCareOf: String = "",
     var presentVillage: String = "",
     var presentPostOffice: String = "",
     var presentUpazila: String = "",
@@ -34,6 +46,7 @@ data class UserProfile(
     var presentPostCode: String = "",
 
     // Address - Permanent
+    var permanentCareOf: String = "",
     var permanentVillage: String = "",
     var permanentPostOffice: String = "",
     var permanentUpazila: String = "",
@@ -41,29 +54,34 @@ data class UserProfile(
     var permanentDivision: String = "",
     var permanentPostCode: String = "",
 
-    // Identity
-    var religion: String = "",
-    var nationality: String = "বাংলাদেশী",
-    var gender: String = "",
-    var maritalStatus: String = "",
-    var quota: String = "",
+    // Education - JSC
+    var jscExam: String = "",
+    var jscBoard: String = "",
+    var jscRollNo: String = "",
+    var jscRegNo: String = "",
+    var jscYear: String = "",
+    var jscGpa: String = "",
+    var jscResultType: String = "",
 
     // Education - SSC
+    var sscExam: String = "S.S.C",
     var sscBoard: String = "",
     var sscRollNo: String = "",
     var sscRegNo: String = "",
     var sscYear: String = "",
     var sscGpa: String = "",
     var sscGroup: String = "",
-    var careOf: String = "",
+    var sscResultType: String = "GPA(out of 5)",
 
     // Education - HSC
+    var hscExam: String = "H.S.C",
     var hscBoard: String = "",
     var hscRollNo: String = "",
     var hscRegNo: String = "",
     var hscYear: String = "",
     var hscGpa: String = "",
     var hscGroup: String = "",
+    var hscResultType: String = "GPA(out of 5)",
 
     // Education - Graduation
     var graduationDegree: String = "",
@@ -71,9 +89,10 @@ data class UserProfile(
     var graduationInstitution: String = "",
     var graduationYear: String = "",
     var graduationResult: String = "",
+    var graduationResultType: String = "",
+    var graduationDuration: String = "",
 
     // Others
-    var bloodGroup: String = "",
     var physicalDisability: String = "না",
     var freedomFighterQuota: String = "না",
 
